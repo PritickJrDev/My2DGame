3da9d8e169c40fec288d6374736849ff9368a026
Prithiraj Mahilary <98103024+PritickJrDev@users.noreply.github.com>
2022-08-07 19:43:04 +0530

    first commit

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -12,22 +12,43 @@ public class Interaction : MonoBehaviour
     bool firstAppleIsSpawned = true;
     bool firstAppleIsDestroyed;
 
+    public GameObject dollHitEffect;
+
+    private Transform targetMagicChest, magicAppleTransform;
+    private Rigidbody2D magicAppleRb;
+    bool canMoveApple, canSpawnApple, isAppleActive;
+
+    private void Awake()
+    {
+        //magicAppleRb = GameObject.FindGameObjectWithTag("MagicApple").GetComponent<Rigidbody2D>();
+        //magicAppleTransform = GameObject.FindGameObjectWithTag("MagicApple").GetComponent<Transform>();
+        targetMagicChest = GameObject.FindGameObjectWithTag("MagicChest").GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMoveApple)
+        {
+            AddMagicApple();
+        }
+    }
+
     private void Update()
     {
-      //  MagicCar();
+        if (Input.GetKeyDown(KeyCode.E) && canSpawnApple == true)
+        {
+            isAppleActive = true;
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Portal") && firstAppleIsSpawned == true)
-        {
-            Instantiate(magicApple1, spawnApplePoint.position, Quaternion.identity);
-            firstAppleIsSpawned = false;
-        }
 
         if (collision.gameObject.CompareTag("MagicApple"))
         {
-            Destroy(collision.gameObject);
+            canMoveApple = true;
+            collision.gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
             firstAppleIsDestroyed = true;
         }
 
@@ -40,15 +61,47 @@ public class Interaction : MonoBehaviour
 
         if (collision.gameObject.CompareTag("MagicDoll"))
         {
+            Instantiate(dollHitEffect, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
             Destroy(collision.gameObject);
+        }
+
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Portal"))
+        {
+            canSpawnApple = true;
+            Debug.Log("Entered");
+        }
+
+        if (collision.gameObject.CompareTag("Portal") && firstAppleIsSpawned == true && isAppleActive)
+        {
+                Instantiate(magicApple1, spawnApplePoint.position, Quaternion.identity);
+                magicAppleRb = GameObject.FindGameObjectWithTag("MagicApple").GetComponent<Rigidbody2D>();
+                magicAppleTransform = GameObject.FindGameObjectWithTag("MagicApple").GetComponent<Transform>();
+                firstAppleIsSpawned = false;
+            
         }
     }
 
-    void MagicCar()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if(firstAppleIsSpawned == false)
+        if (collision.gameObject.CompareTag("Portal"))
         {
-          
+            canSpawnApple = false;
+            Debug.Log("Exited");
         }
+    }
+
+    void AddMagicApple()
+    {
+        Vector2 direction = (Vector2)targetMagicChest.position - magicAppleRb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, magicAppleTransform.transform.up).z;
+        magicAppleRb.angularVelocity = -rotateAmount * 800f;
+        magicAppleRb.velocity = magicAppleTransform.transform.up * 10f;
+
     }
 }
